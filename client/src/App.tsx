bf13d9f948f3cc91c3e655401a00708fdd5bb7e1
Kaustav333 <kaustavkalita9954@gmail.com>
2026-08-21 20:45:14 +0530

    Change root route to Events

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <Navbar />
         <main className="flex-1 w-full max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
           <Routes>
-            <Route path="/" element={<div className="text-center mt-20"><h1 className="text-4xl font-bold text-gray-900">Welcome to Ticketing System</h1></div>} />
+            <Route path="/" element={<Events />} />
             <Route path="/login" element={<Login />} />
             <Route path="/events" element={<Events />} />
             <Route path="/events/:id/seats" element={<SeatMap />} />
