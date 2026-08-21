f93086479f6267419b9444be55d583dd51866bad
Kaustav333 <kaustavkalita9954@gmail.com>
2026-08-21 21:03:45 +0530

    Fix FastAPI CORS wildcard crash

--- server/main.py
+++ server/main.py
@@ -18,7 +18,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title="AuraTix API", lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
